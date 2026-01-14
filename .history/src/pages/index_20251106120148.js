adb738ac4a8c31c27723a43618382d448fc026a2
palaksurana <psurana077@gmail.com>
2026-01-14 21:43:42 +0530

    ignore history folder

--- .history/src/pages/index_20251106120148.js
+++
@@ -1,8 +0,0 @@
-import Home from "./Home"
-import About from "./About"
-import Vision from "./Vision"
-import Events from "./Events"
-import Gallery from "./Gallery"
-import Contacts from "./Contacts"
-
-export { Home, About, Vision, Events, Gallery, Contacts }
